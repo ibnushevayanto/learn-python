6b78ae77d09b43268b6c311498140b246fc4052b
Ibnu Shevayanto <ibnushevayanto@gmail.com>
2022-09-21 22:57:59 +0700

    add day 1

---
+++ Day-1-Band-Name-Generator/main.py
@@ -0,0 +1,5 @@
+print("Selamat Datang Di Aplikasi Nama Band Generator")
+living = input("Dimanakah anda tinggal?\n")
+petName = input("Siapa nama peliharaan anda?\n")
+
+print("Nama Band Anda Yang Cocok Untuk Anda Adalah " + living + " " + petName)
---
+++ Day-1-Band-Name-Generator/readme.md
@@ -0,0 +1,1 @@
+keyword: variabel, input, print.
