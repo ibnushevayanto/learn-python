02f2da64ed8b9eb04023c0a8cdb83c1d18feca16
Ibnu Shevayanto <ibnushevayanto@gmail.com>
2022-10-16 22:00:17 +0700

    add day 15

--- Day-14-Higher-Lower-Game/main.py
+++ Day-14-Higher-Lower-Game/main.py
@@ -1,21 +1,25 @@
+import os
+import random
+
 import art
 import game_data
-import random
-from replit import clear
 
 score = 0
 
+
 def get_text_compare(item):
     return f"{item['name']}, a {item['description']}, from {item['country']}"
+
 
 def compare(itemA, itemB, answer):
     if answer == 'a':
         return itemA['follower_count'] > itemB['follower_count']
     elif answer == 'b':
         return itemB['follower_count'] > itemA['follower_count']
-    else: 
+    else:
         print("Invalid answer")
         return False
+
 
 def game_play():
     itemA = game_data.data[random.randint(0, len(game_data.data) - 1)]
@@ -27,11 +31,12 @@ def game_play():
     answer = input("Who has more followers? Type 'A' or 'B': ").lower()
 
     return compare(itemA, itemB, answer)
-    
+
+
 while True:
-    if(game_play()):
+    if (game_play()):
         score += 1
-        clear()
+        os.system("clear")
         print(f"You're right! current score: {score}")
     else:
         print(f"Sorry, that's wrong. Final score: {score}")
---
+++ Day-15-Coffee-Machine/main.py
@@ -0,0 +1,61 @@
+from data import MENU
+from data import COIN
+
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
+money = 0
+
+
+def make_coffee(command):
+    global money
+    for key in MENU[command]["ingredients"]:
+        if resources[key] < MENU[command]["ingredients"][key]:
+            print(
+                f"{key} not enough. Money refunded.")
+            return False
+
+    money += MENU[command]["cost"]
+    resources['water'] -= MENU[command]["ingredients"]["water"]
+    resources["milk"] -= MENU[command]["ingredients"]['milk'] if 'milk' in MENU[command]["ingredients"] else 0
+    resources["coffee"] -= MENU[command]["ingredients"]["coffee"]
+
+    print(f"Here is your {command} ☕️. Enjoy!")
+
+
+def show_report():
+    print(
+        f"Water: {resources['water']}\nMilk: {resources['milk']}\nCoffee: {resources['coffee']}\nMoney: {money}"
+    )
+
+
+def inputMoney():
+    total_quarters = int(input("How many quarters?: "))
+    total_dimes = int(input("How many dimes?: "))
+    total_nickles = int(input("How many nickles?: "))
+    total_pennies = int(input("How many pennies?: "))
+    return (total_quarters * COIN["quarter"]) + (total_dimes * COIN["dime"]) + (
+        total_nickles * COIN["nickel"]) * (total_pennies * COIN["penny"])
+
+
+while True:
+    command = input(
+        "What would you like? (espresso/latte/cappuccino): ").lower()
+    if command == "report":
+        show_report()
+    elif command == "off":
+        break
+    else:
+        if command in MENU:
+            total_money = inputMoney()
+            if total_money >= MENU[command]['cost']:
+                if total_money - MENU[command]['cost'] > 0:
+                    if make_coffee(command) != False:
+                        print(
+                            f"Here is ${total_money - MENU[command]['cost']} in change")
+            else:
+                print("Sorry that's not enough money. Money refunded.")
+        else:
+            print("Command not found")
